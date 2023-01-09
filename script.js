5f01e298b16d6aa75c76a1ce0327dcb23d0b5222
obed300 <obamfo@st.ug.edu.gh>
2023-01-09 10:38:32 +0000

    Function to add book to book list

---
+++ script.js
@@ -0,0 +1,12 @@
+const bookDisplay = document.querySelector('.book-display');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+
+
+const bookList = [];
+
+function addBook(title,author){
+ bookList.push({title,author})
+}
+
+
